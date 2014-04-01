840a1c5dc7de04176dd28bd365b1432f86a445c2
Ezequiel Lovelle <ezequiellovelle@gmail.com>
2014-04-01 14:56:07 -0300

    changes

--- ica/tests/functional/test_api.py
+++
@@ -1,7 +0,0 @@
-from ica.tests import *
-
-class TestApiController(TestController):
-
-    def test_index(self):
-        response = self.app.get(url(controller='api', action='index'))
-        # Test response...
