add26f4662b729ea6fac4d7f7ac75400bfb6929c
Ezequiel Lovelle <ezequiellovelle@gmail.com>
2014-05-07 13:55:50 -0300

    stupid changes

--- ica/controllers/main.py
+++ ica/controllers/main.py
@@ -1,10 +1,11 @@
 import logging
 
 from pylons import request, response, session, tmpl_context as c, url
+from pylons import app_globals as g
 from pylons.controllers.util import abort, redirect
+
 from ica.lib.base import BaseController
 from ica.lib.util import pjax
-from pylons import app_globals as g
 from datetime import datetime, timedelta
 
 log = logging.getLogger(__name__)
