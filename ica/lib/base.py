4de6797dec919fe7e5613a94a01358ec67ee3184
Ezequiel Lovelle <ezequiellovelle@gmail.com>
2014-05-09 13:30:42 -0300

    debug render time of controllers

--- ica/lib/base.py
+++ ica/lib/base.py
@@ -2,10 +2,20 @@
 
 Provides the BaseController class for subclassing.
 """
+import logging
+import time
+
+from pylons import request, response, session, tmpl_context as c
 from pylons.controllers import WSGIController
 from pylons.templating import render_jinja2 as render
 
+log = logging.getLogger(__name__)
+
 class BaseController(WSGIController):
+
+    def __init__(self):
+    	c.timer = time.time()
+
 
     def __call__(self, environ, start_response):
         """Invoke the Controller"""
@@ -13,3 +23,9 @@ class BaseController(WSGIController):
         # the request is routed to. This routing information is
         # available in environ['pylons.routes_dict']
         return WSGIController.__call__(self, environ, start_response)
+
+
+    def __after__(self):
+    	end_timer = time.time() - c.timer
+    	url = request.environ['PATH_INFO']
+    	log.debug("'%s' render time %.3f seconds" % (url, end_timer))
